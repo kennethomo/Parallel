5590d987a1d403b79993a616d677f79054ec41f1
Kenneth <kennethomo830@gmail.com>
2020-04-04 12:08:02 -0500

    removed .sh files

--- Project3/sendLone.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-scp FractalDir/fractal_pthread.cpp kjo15@ls5.tacc.utexas.edu:/home1/07162/kjo15/Project3/FractalDir
-#scp CollatzDir/collatz_pthread.cpp kjo15@ls5.tacc.utexas.edu:/home1/07162/kjo15/Project2/CollatzDir
--- Project4/sendLone.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-scp cpScript.sh kjo15@ls5.tacc.utexas.edu:/home1/07162/kjo15/Project4
-#scp CollatzDir/collatz_pthread.cpp kjo15@ls5.tacc.utexas.edu:/home1/07162/kjo15/Project2/CollatzDir
